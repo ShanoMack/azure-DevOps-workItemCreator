0a097ff94af5635ff2fb9d1b3fa31f08b280ba41
ImShaneTurner <shaneturner@live.co.za>
2025-05-14 10:51:45 +0200

    hopefully fixed build issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
 
-// CHANGE THIS to your actual GitHub repo name
-const repoName = "azure-DevOps-workItemCreator"
-
 export default defineConfig(({ mode }) => ({
-  base: `/${repoName}/`, // 👈 This is what you need for GitHub Pages
+  base: mode === "production" ? "/azure-DevOps-workItemCreator/" : "/",
   server: {
     host: "::",
     port: 8080,
