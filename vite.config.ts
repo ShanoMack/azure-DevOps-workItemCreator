4d3556967defff95f556d4e0586998baf75859ee
ImShaneTurner <shaneturner@live.co.za>
2025-05-14 09:40:20 +0200

    Trying to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
-// https://vitejs.dev/config/
+// CHANGE THIS to your actual GitHub repo name
+const repoName = "azure-DevOps-workItemCreator"
+
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`, // 👈 This is what you need for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+}))
