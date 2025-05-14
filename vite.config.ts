9fb52cb35dd4062fbadbcd8b6fb9672844a3ab73
ImShaneTurner <shaneturner@live.co.za>
2025-05-14 14:54:15 +0200

    pushed new URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 import path from "path"
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/azure-DevOps-workItemCreator/" : "/",
+  base: mode === "production" ? "/azure-devOps-forge/" : "/",
   server: {
     host: "::",
     port: 8080,
